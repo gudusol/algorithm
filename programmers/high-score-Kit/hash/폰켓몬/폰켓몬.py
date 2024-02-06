76a287d0b329ab25e88b0caab647c05b4d8289a7
taeseung-yoo <remicon99@gmail.com>
2024-02-06 12:35:43 +0900

    hash - 폰켓몬

--- programmers/high-score-Kit/hash/폰켓몬/폰켓몬.py
+++ programmers/high-score-Kit/hash/폰켓몬/폰켓몬.py
@@ -1,1 +1,9 @@
-print("hello")
+def solution(nums):
+    dic = {}
+    for num in nums:
+        if num in dic:
+            dic[num] = dic[num] + 1
+        else:
+            dic[num] = 1
+
+    return min(len(dic), len(nums) // 2)
