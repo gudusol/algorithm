f2b7b9ed84cda645f32dc85c1dc59bec421af1c0
Yoo TaeSeung <59465914+taeseung-yoo@users.noreply.github.com>
2024-02-09 17:42:53 +0900

    [level 3] Title: 베스트앨범, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579#) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.02 ms
+메모리: 10.3 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 5일 17:34:28
+2024년 2월 5일 17:42:51
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -10,17 +10,10 @@ def solution(genres, plays):
             dic[genre]['total'] += play
             dic[genre]['list'].append(id)
         else:
-            dic[genre] = {"total": play, "list": [id]}
-            
+            dic[genre] = {"total": play, "list": [id]}           
     sorted_dict = sorted(dic.items(), key = lambda x:x[1]['total'], reverse=True)
 
     for genre, value in sorted_dict:
         id_list = value['list']
-        
-        count = 0
-        for id in id_list:
-            if count == 2:
-                break
-            ans.append(id)
-            count += 1
+        ans += id_list[:min(len(id_list), 2)]
     return ans
