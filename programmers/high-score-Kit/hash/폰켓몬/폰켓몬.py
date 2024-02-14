e9163fca4fbf99c9fad3987839ce7e5e46943033
Yoo TaeSeung <59465914+taeseung-yoo@users.noreply.github.com>
2024-02-15 02:11:47 +0900

    Delete programmers/high-score-Kit/hash directory

--- programmers/high-score-Kit/hash/폰켓몬/폰켓몬.py
+++
@@ -1,9 +0,0 @@
-def solution(nums):
-    dic = {}
-    for num in nums:
-        if num in dic:
-            dic[num] = dic[num] + 1
-        else:
-            dic[num] = 1
-
-    return min(len(dic), len(nums) // 2)
