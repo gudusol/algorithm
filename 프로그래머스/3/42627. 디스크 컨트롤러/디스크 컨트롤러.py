fd96a1c27f09e6219df77507c64f0a50cd732c56
Yoo TaeSeung <59465914+taeseung-yoo@users.noreply.github.com>
2024-02-20 16:53:38 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -0,0 +1,29 @@
+import heapq as hq
+
+def solution(jobs):
+    hq.heapify(jobs) # 시작시간 오름차순
+    count = len(jobs)
+    answer = 0
+    t = 0
+    tasks = [] # 소요시간 오름차순
+    while len(jobs) > 0 or len(tasks) > 0:
+        while len(jobs) > 0:
+            start, time = hq.heappop(jobs)
+            if (t >= start):
+                hq.heappush(tasks, [time, start]) # 소요시간 짧은 순
+            else:
+                hq.heappush(jobs, [start, time]) 
+                break
+        
+        if len(tasks) > 0 :
+            time, start = hq.heappop(tasks)
+            t += time
+            answer += (t - start)     
+            print(answer)
+        else:
+            start, time = hq.heappop(jobs)
+            t = start + time
+            answer += (t - start)     
+            print(answer)
+    
+    return answer // count
