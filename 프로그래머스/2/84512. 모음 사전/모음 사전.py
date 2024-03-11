4e22f3489c2b9275177e4ad90cde47e2865a8d25
Yoo TaeSeung <59465914+taeseung-yoo@users.noreply.github.com>
2024-03-11 21:30:35 +0900

    [level 2] Title: 모음 사전, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -0,0 +1,11 @@
+def solution(word):
+    seq = {'A': 0, 'E': 1, 'I': 2, 'O': 3, 'U': 4}
+    answer = 0
+    for idx, w in enumerate(word.ljust(5, '0')):
+        if w != '0':
+            answer += 1
+            for _ in range(seq[w]):
+                for power in range(5 - idx):
+                    answer += 5 ** power
+                
+    return answer
