0743162c1d9fb4cf39fee5ee8b39c7c03cbe4633
Yoo TaeSeung <59465914+taeseung-yoo@users.noreply.github.com>
2024-04-01 23:29:04 +0900

    [level 3] Title: 선입 선출 스케줄링, Time: 6.22 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.py
@@ -0,0 +1,28 @@
+def solution(n, cores):
+    n -= len(cores)
+    if n <= 0:
+        return 0
+    lo = 0
+    hi = n * max(cores)
+    
+    while lo + 1 < hi:
+        p = 0
+        mid = (lo + hi) // 2
+        for c in cores:
+            p += mid // c
+            
+        if p >= n:
+            hi = mid
+        else:
+            lo = mid
+    for idx, c in enumerate(cores):
+        n -= lo // c
+        remain = hi % c
+        cores[idx] = remain if remain == 0 else c - remain
+    
+    for idx, c in enumerate(cores):
+        if c == 0:
+            n -= 1
+            if n == 0:
+                return idx + 1
+    return 
