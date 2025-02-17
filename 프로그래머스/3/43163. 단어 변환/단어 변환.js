55119e68f33ae04e886ccb7cabfbfe05e01b61a2
TaeSeung Yoo <59465914+gudusol@users.noreply.github.com>
2025-02-17 10:19:32 +0900

    [level 3] Title: 단어 변환, Time: 0.39 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,39 +1,30 @@
-function getDifferenceAlphabet (word1, word2) {
+function checkAlphabet (word1, word2) {
     let difference = 0;
     for (let i = 0; i < word1.length; i ++){
         if (word1[i] !== word2[i]){
             difference ++;
         }
     }
-    return difference;
-}
-
-function bfs(target, words, current_str, current_count, visited, answer) {
-    if (target === current_str) {
-        answer.push(current_count);
-        return;
-    }
-    
-    for(let i = 0; i < words.length; i++){
-        if (!visited[i] && getDifferenceAlphabet(current_str, words[i]) === 1){
-            const new_visited = visited.slice();
-            new_visited[i] = true;
-            bfs(target, words, words[i], current_count + 1, new_visited, answer);
-        }
-    }
+    return difference === 1;
 }
 
 
 function solution(begin, target, words) {
     if (!words.includes(target)) return 0;
-    let count = 0;
     const visited = Array.from({length: words.length}, () => false);
-    const answer = [];
+    const queue = [[begin, 0]];
     
-    bfs(target, words, begin, count, visited, answer);
-    if (answer.length > 0){
-        return Math.min(...answer);
-    } else {
-        return 0;
+    while (queue.length > 0){
+        const [cur_str, cur_count] = queue.shift();
+        if (cur_str === target){
+            return cur_count;
+        }
+        
+        for (let i = 0; i < words.length; i ++){
+            if(!visited[i] && checkAlphabet(cur_str, words[i])) {
+                visited[i] = true;
+                queue.push([words[i], cur_count + 1]);
+            }
+        }
     }
 }
