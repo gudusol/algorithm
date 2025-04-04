6268eaa0e92633bd15b6e52186499e6348f6cf44
TaeSeung Yoo <59465914+gudusol@users.noreply.github.com>
2025-04-04 18:50:44 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,8 @@
+import sys
+from itertools import combinations
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+
+for c in combinations(range(1, N + 1), M):
+    print(*list(c), sep=" ")
