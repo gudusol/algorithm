f29a7b1c764628c5e4340d0ea53c4647e48e3d3b
TaeSeung Yoo <59465914+gudusol@users.noreply.github.com>
2025-10-08 18:34:00 +0900

    [Gold II] Title: 성냥개비, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Gold/3687. 성냥개비/성냥개비.py
@@ -0,0 +1,40 @@
+import sys
+import math
+
+input = sys.stdin.readline
+
+T = int(input())
+numbers = [int(input()) for _ in range(T)]
+dp = [math.inf for _ in range(max(max(numbers) + 1, 8))]
+
+num_count = {2: "1", 3: "7", 4: "4", 5: "2", 6: "0", 7: "8"}
+
+for c in num_count:
+    dp[c] = int(num_count[c])
+dp[6] = "6"
+dp[8] = "10"
+
+for i in range(9, len(dp)):
+    for k, v in num_count.items():
+        if k == 6:
+            dp[i] = str(
+                min(dp[i] if dp[i] == math.inf else int(dp[i]), int(str(dp[i - k]) + v))
+            )
+        else:
+            dp[i] = str(
+                min(
+                    dp[i] if dp[i] == math.inf else int(dp[i]),
+                    int(str(dp[i - k]) + v),
+                    int(v + str(dp[i - k])),
+                )
+            )
+
+for num in numbers:
+    answer_max = ""
+
+    if num % 2 == 0:
+        answer_max = "1" * (num // 2)
+    else:
+        answer_max = "7" + "1" * (num // 2 - 1)
+
+    print(dp[num], answer_max)
